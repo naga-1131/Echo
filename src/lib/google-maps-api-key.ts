59fe41cd3b90128c0311bb3ce030474e20ccb575
NAGAIAH VASUNURI <princevsn01@gmail.com>
2025-09-07 02:25:12 +0000

    File changes

--- src/lib/google-maps-api-key.ts
+++ src/lib/google-maps-api-key.ts
@@ -1,3 +1,3 @@
 // Please replace 'YOUR_GOOGLE_MAPS_API_KEY' with your actual Google Maps API key.
 // You can get a key from the Google Cloud Console: https://console.cloud.google.com/google/maps-apis
-export const API_KEY = "YOUR_GOOGLE_MAPS_API_KEY";
+export const API_KEY = "AIzaSyBI5AJV5xOmA3ECWZ6bK2MiHRToWruogiA";
